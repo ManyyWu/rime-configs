d0c8c336cd61acea013f2399e0772d61154fb204
Manyy <manyy.openkt@gmail.com>
2026-09-08 14:13:25 +0800

    v1

--- Android-Trime/push.manyy.trime.yaml.sh
+++ Android-Trime/push.manyy.trime.yaml.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-adb -t 5 push manyy.trime.yaml /storage/emulated/0/Android/data/com.osfans.trime/files/rime/manyy.trime.yaml
+adb -t $1 push manyy.trime.yaml /storage/emulated/0/Android/data/com.osfans.trime/files/rime/manyy.trime.yaml
